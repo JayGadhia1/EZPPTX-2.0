0c1f4456f87019609deaaf940ce007753300c05a
JUSTINPARDO <100895234+JUSTINPARDO@users.noreply.github.com>
2024-10-06 21:33:44 -0500

    Update start.js

--- convert/scripts/start.js
+++ convert/scripts/start.js
@@ -1,23 +1,12 @@
 const concurrently = require('concurrently');
 const path = require('path');
-const dotenv = require('dotenv');
-
-dotenv.config({ path: path.join(__dirname, '..', 'server', 'pybackend', '.env') });
 
 const pythonPath = path.join(__dirname, '..', 'server', 'pybackend', 'venv', 'bin', 'python');
 const serverPath = path.join(__dirname, '..', 'server', 'pybackend', 'server.py');
 
 concurrently([
   { command: 'node server/server.js', name: 'node-server', prefixColor: 'blue' },
-  { 
-    command: `${pythonPath} ${serverPath}`,
-    name: 'python-server',
-    prefixColor: 'green',
-    env: {
-      ...process.env,
-      PYTHONUNBUFFERED: '1'
-    }
-  },
+  { command: `${pythonPath} ${serverPath}`, name: 'python-server', prefixColor: 'green' },
   { command: 'ccweb-add-on-scripts start', name: 'addon', prefixColor: 'yellow' }
 ], {
   prefix: 'name',
